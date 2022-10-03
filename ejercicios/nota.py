480b2d7503a2a2b24d8860655fbcee29c2fe2078
mikygaleano <creatormikeonline@gmail.com>
2022-10-03 11:21:37 -0300

    pythonUndav

---
+++ ejercicios/nota.py
@@ -0,0 +1,28 @@
+# 20.- Escriba un programa que valide si una nota está entre 0 y 10, sino está entre 0 y 10 la nota
+# se pedirá de nuevo hasta que la nota sea correcta.
+
+nota = int(input(f'Ingrese la nota, por favor: '))
+
+
+i = 0
+while nota < 0 or nota > 10:
+    print('Nota invalida')
+    nota = int(input('Por favor vuelva a ingresar una nota valida: '))
+    i =+ 1
+    j = 0
+    while nota >= 0 and nota <= 10:
+        print('Nota valida')
+        j += 1
+        break
+
+
+
+    
+
+    
+
+
+
+
+
+
